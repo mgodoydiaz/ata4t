c35b0a6b3db2e3cc282f8881726a7f2cb2e3b8ca
mgodoydiaz <magodoydiaz@gmail.com>
2025-02-09 17:34:39 -0300

    Integrates versioneer

--- ata4t/__init__.py
+++ ata4t/__init__.py
@@ -3,3 +3,13 @@
 
 import os
 from ata4t.tools import connection_tools, file_tools, xAPIConnector
+
+ATA4T_PATH = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+
+# Handle versioneer
+from ._version import get_versions
+
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
--- ata4t/tools/connection_tools.py
+++ ata4t/tools/connection_tools.py
@@ -1,24 +1,27 @@
 from .xAPIConnector import *
 from . import file_tools
+#from xAPIConnector import *
+#import file_tools
+def login(userId, password):
+    """En base a un archivo json de credenciales se loguea en la API de xAPI"""
 
-# Cargar credenciales
-credentials = file_tools.read_credentials('credentials.json')
+    # Crear una instancia de cliente de API
+    client = APIClient()
 
-# Credenciales de inicio de sesión
-userId = credentials['userID']
-password = credentials['password']
-
-# Crear una instancia de cliente de API
-client = APIClient()
-
-if __name__ == '__main__':
-
-    # Intenta iniciar sesión
     login_response = client.execute(loginCommand(userId=userId, password=password))
     if not login_response.get('status'):
-        print("Login failed:", login_response.get('errorCode'))
-    else:
-        print("Login successful")
-        streamSessionId = login_response.get('streamSessionId')
+        raise(Exception("Login failed:", login_response.get('errorCode')))
+    return client, login_response.get('streamSessionId')
 
+if __name__ == '__main__':
+    
+    # Cargar credenciales
+    credentials = file_tools.read_credentials('../../credentials.json')
+
+    # Credenciales de inicio de sesión
+    userId = credentials['userID']
+    password = credentials['password']
+
+    _, streamSessionId = login(userId, password)
+    print("StreamSessionId:", streamSessionId)
 
