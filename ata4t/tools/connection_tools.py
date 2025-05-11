a0d45cb583b6e1142ffee50861c504aa6aa0b50d
mgodoydiaz <magodoydiaz@gmail.com>
2025-05-11 16:01:52 -0400

    Change in connection_tools

--- ata4t/tools/connection_tools.py
+++ ata4t/tools/connection_tools.py
@@ -1,17 +1,17 @@
 from .xAPIConnector import *
 from . import file_tools
 
-# Cargar credenciales
-credentials = file_tools.read_credentials('credentials.json')
-
-# Credenciales de inicio de sesión
-userId = credentials['userID']
-password = credentials['password']
-
-# Crear una instancia de cliente de API
-client = APIClient()
-
 if __name__ == '__main__':
+
+    # Cargar credenciales
+    credentials = file_tools.read_credentials('credentials.json')
+
+    # Credenciales de inicio de sesión
+    userId = credentials['userID']
+    password = credentials['password']
+
+    # Crear una instancia de cliente de API
+    client = APIClient()
 
     # Intenta iniciar sesión
     login_response = client.execute(loginCommand(userId=userId, password=password))
